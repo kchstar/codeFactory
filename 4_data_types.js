031e24ac8c51a6cdbae6d72c346f9389fb665744
kchstar_outlook_com <kchstar@outlook.com>
2024-03-25 11:41:13 +0900

    first commit

---
+++ 4_data_types.js
@@ -0,0 +1,83 @@
+
+
+
+
+
+
+
+const age = 32;
+const temper = -10;
+const pi = 3.14;
+
+
+
+
+
+/**
+ * String 타입
+ */
+
+const codeFactory = '코드팩토리';
+console.log(codeFactory);
+console.log(typeof codeFactory);
+
+
+
+
+
+
+
+
+
+
+
+const iveYuJin = '아이브\n안유진'; 
+console.log(iveYuJin);
+const iveWonYoung = '아이브\t장원영\n김천호';
+console.log(iveWonYoung);
+console.log('-----------')
+const backSlash = '아이브\\코드팩토리1';
+console.log(backSlash);
+console.log('-----------')
+const smallQutoation = '아이브\'코드팩토리2';
+console.log(smallQutoation);
+console.log('-----------')
+const iveWonYoung2 = `아이브i //////
+장원영`;
+console.log(iveWonYoung2);
+console.log('-----------')
+const groupName = '아이브';
+console.log(groupName + '안유진');
+console.log(`${groupName}안유진`);
+console.log('-----------')
+const isTrue = true;
+const isFalse = false;
+console.log(typeof isTrue);
+console.log(typeof isFalse);
+console.log('-----------')
+let noInit;
+console.log(noInit);
+console.log(typeof noInit);
+console.log('-----------')
+let init = null;
+console.log(init);
+console.log(init);
+console.log(typeof init);
+
+console.log('-----------')
+
+console.log('-----------')
+const dictionary = {
+    red:'빨강색',
+    orange: '주황색',
+    yellow: '노랑색',
+};
+console.log(dictionary);
+console.log('-----------')
+console.log(dictionary ['red']);
+console.log(dictionary ['yellow']);
+console.log(typeof dictionary);
+console.log('-----------')
+
+
+console.log('-----------')
---
+++ 7_operator.js
@@ -0,0 +1,13 @@
+
+
+
+
+console.log(5 == 5);
+console.log(5 == '5');
+console.log(0 == '');
+console.log(true == 1);
+console.log(false == 0);
+console.log('--------')
+console.log(5 == 5);
+console.log(5 == 5);
+console.log(5 == 5);
